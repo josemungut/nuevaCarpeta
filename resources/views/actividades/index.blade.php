e5a17ea207af91992efd33cfb65789e06953f795
Jose Muñoz Gutierrez <jmungut28@iesmarquesdecomares.org>
2023-06-13 21:13:35 +0200

    .

--- resources/views/actividades/index.blade.php
+++ resources/views/actividades/index.blade.php
@@ -60,29 +60,49 @@
                                         type="button"
                                         class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Detalle</button></a>
                                 @if ($clase->sala->aforo > 0)
-                                    <form class="inline-block" action={{ route('reserva.store') }} method="POST">
-                                        @csrf
-                                        @method('POST')
-                                        <input type="hidden" name="id_actividad" value="{{ $clase->id }}"> <button
-                                            type="submit" id="botonreservar"
-                                            class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">RESERVAR</button>
-                                    </form>
-                                @endif
-                                @if (Auth::user()->tipo == 'admin')
-                                    <a href="{{ route('actividades.edit', ['actividade' => $clase->id]) }}"><button
-                                            type="button" c
-                                            class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Editar</button></a>
-                                    <form class="inline-block"
-                                        action={{ route('actividades.destroy', ['actividade' => $clase->id]) }}
-                                        method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                            class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-400 dark:text-red-400 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-900">Borrar</button>
-                                    </form>
+                                    @php
+                                        $reservada = false;
+                                    @endphp
+                                    @foreach ($clase->reserva as $reserva)
+                                        @php
+                                            if ($reserva->id_users == Auth::user()->id) {
+                                                $reservada = true;
+                                            }
+                                        @endphp
+                                    @endforeach
+                                    @if ($reservada)
+                                        <form class="inline-block" action={{ route('reserva.store') }} method="POST">
+                                            @csrf
+                                            @method('POST')
+                                            <button type="submit" disabled
+                                                class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">Clase
+                                                Reservada</button>
+                                        </form>
+                                    @else
+                                        <form class="inline-block" action={{ route('reserva.store') }} method="POST">
+                                            @csrf
+                                            @method('POST')
+                                            <input type="hidden" name="id_actividad" value="{{ $clase->id }}">
+                                            <button type="submit" id="botonreservar"
+                                                class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">RESERVAR</button>
+                                        </form>
+                                    @endif
+                                    @if (Auth::user()->tipo == 'admin')
+                                        <a href="{{ route('actividades.edit', ['actividade' => $clase->id]) }}"><button
+                                                type="button" c
+                                                class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Editar</button></a>
+                                        <form class="inline-block"
+                                            action={{ route('actividades.destroy', ['actividade' => $clase->id]) }}
+                                            method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit"
+                                                class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-400 dark:text-red-400 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-900">Borrar</button>
+                                        </form>
+                                    @endif
                                 @endif
 
-                                
+
                             </td>
                         </tr>
                     @endforeach
